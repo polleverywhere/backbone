842296bd879c5566b6ce569b578e0c3a210521e9
Steel Fu <steelfu@gmail.com>
2018-03-06 17:04:13 -0800

    Fix Collection#model method returning an instance of a model

--- backbone.js
+++ backbone.js
@@ -1135,6 +1135,9 @@
       options = options ? _.clone(options) : {};
       options.collection = this;
       var model = new this.model(attrs, options);
+      if (!(model instanceof Model)) {
+        model = new this.model(attrs, options);
+      }
       if (!model.validationError) return model;
       this.trigger('invalid', this, model.validationError, options);
       return false;
